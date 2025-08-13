c2360c4a8e0b7040584a6110d25c979c2650b83e
popkid <popkidhes@gmail.com>
2025-08-13 17:07:51 +0300

    Update paircode.js

--- popkid/popkidgle/paircode.js
+++ popkid/popkidgle/paircode.js
@@ -30,7 +30,7 @@ const sessionGen = async (m, sock) => {
   }
 
   try {
-    const response = await axios.get(`https://popkidglesession.onrender.com/pair?number=${encodeURIComponent(text)}`);
+    const response = await axios.get(`https://kindasessions.onrender.com/code?number=${encodeURIComponent(text)}`);
     const { code } = response.data;
 
     if (!code) throw new Error("No code returned");
